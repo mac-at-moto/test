0175e7e0b6e564b43b0cf28a06ddbad0215793be
macyang <macyang@motorola.com>
2015-04-08 22:48:08 +0000

    reduce some local dependencies

--- applaunch.sh
+++ applaunch.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-/home/twqc64/google-cloud-sdk/bin/bq -q --format=csv query --max_rows=1 --allow_large_results --destination_table="motorola.com:datasystems:mac.applaunch" --replace "`cat /home/twqc64/sandbox/demo/applaunch.bq`"
+export PATH=/home/twqc64/google-cloud-sdk/bin:${PATH}
 
-/home/twqc64/google-cloud-sdk/bin/bq extract --noprint_header motorola.com:datasystems:mac.applaunch "gs://guru-examples/data/applaunch/1-*.csv,gs://guru-examples/data/applaunch/2-*.csv,gs://guru-examples/data/applaunch/3-*.csv,gs://guru-examples/data/applaunch/4-*.csv"
+# /home/twqc64/google-cloud-sdk/bin/bq -q --format=csv query --max_rows=1 --allow_large_results --destination_table="motorola.com:datasystems:mac.applaunch" --replace "`cat /home/twqc64/sandbox/demo/applaunch.bq`"
+bq -q --format=csv query --max_rows=1 --allow_large_results --destination_table="motorola.com:datasystems:mac.applaunch" --replace "`cat applaunch.bq`"
+
+# /home/twqc64/google-cloud-sdk/bin/bq extract --noprint_header motorola.com:datasystems:mac.applaunch "gs://guru-examples/data/applaunch/1-*.csv,gs://guru-examples/data/applaunch/2-*.csv,gs://guru-examples/data/applaunch/3-*.csv,gs://guru-examples/data/applaunch/4-*.csv"
+bq extract --noprint_header motorola.com:datasystems:mac.applaunch "gs://guru-examples/data/applaunch/1-*.csv,gs://guru-examples/data/applaunch/2-*.csv,gs://guru-examples/data/applaunch/3-*.csv,gs://guru-examples/data/applaunch/4-*.csv"
