5326eef1922dfa0e8e220ac8cb8876220235387a
Mac <macyang@motorola.com>
2015-05-23 08:59:14 -0700

    Update applaunch.sh

--- applaunch.sh
+++ applaunch.sh
@@ -7,3 +7,5 @@ bq -q --format=csv query --max_rows=1 --allow_large_results --destination_table=
 
 # /home/twqc64/google-cloud-sdk/bin/bq extract --noprint_header motorola.com:datasystems:mac.applaunch "gs://guru-examples/data/applaunch/1-*.csv,gs://guru-examples/data/applaunch/2-*.csv,gs://guru-examples/data/applaunch/3-*.csv,gs://guru-examples/data/applaunch/4-*.csv"
 bq extract --noprint_header motorola.com:datasystems:mac.applaunch "gs://guru-examples/data/applaunch/1-*.csv,gs://guru-examples/data/applaunch/2-*.csv,gs://guru-examples/data/applaunch/3-*.csv,gs://guru-examples/data/applaunch/4-*.csv"
+
+
